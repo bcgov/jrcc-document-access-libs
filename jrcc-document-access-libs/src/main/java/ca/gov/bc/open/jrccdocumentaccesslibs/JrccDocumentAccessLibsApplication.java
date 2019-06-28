9e62559cd87619d82f1850ed52a8b4c798e01269
alexjoybc <alexandre.joyeux@gov.bc.ca>
2019-06-28 12:07:25 -0700

    Adding initializr app

---
+++ jrcc-document-access-libs/src/main/java/ca/gov/bc/open/jrccdocumentaccesslibs/JrccDocumentAccessLibsApplication.java
@@ -0,0 +1,13 @@
+package ca.gov.bc.open.jrccdocumentaccesslibs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrccDocumentAccessLibsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JrccDocumentAccessLibsApplication.class, args);
+	}
+
+}
