30613aa39b8c904bf08afcad78b2fe26eae61f2b
alexjoybc <alexandre.joyeux@gov.bc.ca>
2019-06-28 15:57:14 -0700

    throw exception for sentinel mode

--- jrcc-access-spring-boot-autoconfigure/src/main/java/ca/gov/bc/open/jrccaccessspringbootautoconfigure/config/AccessAutoConfiguration.java
+++ jrcc-access-spring-boot-autoconfigure/src/main/java/ca/gov/bc/open/jrccaccessspringbootautoconfigure/config/AccessAutoConfiguration.java
@@ -1,5 +1,7 @@
 package ca.gov.bc.open.jrccaccessspringbootautoconfigure.config;
 
+import javax.naming.OperationNotSupportedException;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +10,7 @@ import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.jca.cci.connection.NotSupportedRecordFactory;
 
 @Configuration
 @EnableConfigurationProperties(RedisProperties.class)
@@ -17,15 +20,18 @@ public class AccessAutoConfiguration {
 	 * Configure the JedisConnectionFactory
 	 * @param properties The redis properties
 	 * @return a JedisConnectionFactory
+	 * @throws OperationNotSupportedException 
 	 */
 	@Bean
 	@ConditionalOnMissingBean(JedisConnectionFactory.class)
-	public JedisConnectionFactory jedisConnectionFactory(RedisProperties properties) {
+	public JedisConnectionFactory jedisConnectionFactory(RedisProperties properties) throws OperationNotSupportedException {
 		
 		switch (properties.getMode()) {
 		case CLUSTER:
 			RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration(properties.getClusterHostAndPort());
 			return new JedisConnectionFactory(redisClusterConfiguration);
+		case SANTINEL:
+			throw new OperationNotSupportedException("Santinel mode is not supported");
 		default:
 			RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(properties.getHost(), properties.getPort());
 			return new JedisConnectionFactory(redisStandaloneConfiguration);
