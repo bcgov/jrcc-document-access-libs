2e72b7dcdd7663ba8b431480ca3e83507a17dbac
alexjoybc <alexandre.joyeux@gov.bc.ca>
2019-07-16 09:35:16 -0700

    feat(plugin): changing queue namespaces

--- jrcc-access-spring-boot-autoconfigure/src/main/java/ca/gov/bc/open/jrccaccess/autoconfigure/plugins/rabbitmq/RabbitMqParam.java
+++ jrcc-access-spring-boot-autoconfigure/src/main/java/ca/gov/bc/open/jrccaccess/autoconfigure/plugins/rabbitmq/RabbitMqParam.java
@@ -8,14 +8,20 @@ package ca.gov.bc.open.jrccaccess.autoconfigure.plugins.rabbitmq;
  */
 public class RabbitMqParam {
 
+	
+	
 	public static final String DOCUMENT_READY_TOPIC = "document.ready";
-	public static final String DOCUMENT_READY_EXTENSION = ".q";
-	public static final String DOCUMENT_READY_DLQ = DOCUMENT_READY_TOPIC + ".dlq";
-	public static final String DOCUMENT_READY_DLX = DOCUMENT_READY_TOPIC + ".dlx";
+	
+	
+	public static final String DOCUMENT_READY_BASE_FORMAT = "{0}.{1, number,#}.x{2, number,#}";
+	public static final String DOCUMENT_READY_Q_FORMAT = DOCUMENT_READY_BASE_FORMAT + ".q";
+	public static final String DOCUMENT_READY_DLQ_FORMAT = DOCUMENT_READY_BASE_FORMAT + ".dlq";
+	public static final String DOCUMENT_READY_DLX_FORMAT = DOCUMENT_READY_BASE_FORMAT + ".dlx";
 	
 	public static final String X_DEAD_LETTER_EXCHANGE_ARG = "x-dead-letter-exchange";
 	public static final String X_DEAD_LETTER_ROUTING_KEY_ARG = "x-dead-letter-routing-key";
 	public static final String X_MESSAGE_TTL_ARG = "x-message-ttl";
+	public static final String X_EXPIRES_ARG = "x-expires";
 	
 	public static final String X_DEAD_LETTER_ROUTING_KEY = "message-ready-dead";
 	
