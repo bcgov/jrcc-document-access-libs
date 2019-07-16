97b0795842322ac927bbaa655f114e1956397ed3
alexjoybc <alexandre.joyeux@gov.bc.ca>
2019-07-16 07:55:51 -0700

    fix(plugin): fixing issue with redis service registration

--- jrcc-access-spring-boot-autoconfigure/src/main/java/ca/gov/bc/open/jrccaccess/autoconfigure/plugins/rabbitmq/RedisStorageService.java
+++ jrcc-access-spring-boot-autoconfigure/src/main/java/ca/gov/bc/open/jrccaccess/autoconfigure/plugins/rabbitmq/RedisStorageService.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 
 import javax.xml.bind.DatatypeConverter;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.cache.Cache.ValueWrapper;
 import org.springframework.cache.CacheManager;
 import org.springframework.data.redis.RedisConnectionFailureException;
@@ -25,7 +25,7 @@ import ca.gov.bc.open.jrccaccess.libs.services.exceptions.ServiceUnavailableExce
  *
  */
 @Service
-@ConditionalOnProperty(name="bcgov.access.output.plugin", havingValue = "rabbitmq")
+@ConditionalOnExpression("'${bcgov.access.input.plugin}' == 'rabbitmq' || '${bcgov.access.output.plugin}' == 'rabbitmq'")
 public class RedisStorageService implements StorageService {
 
 	CacheManager cacheManager;
