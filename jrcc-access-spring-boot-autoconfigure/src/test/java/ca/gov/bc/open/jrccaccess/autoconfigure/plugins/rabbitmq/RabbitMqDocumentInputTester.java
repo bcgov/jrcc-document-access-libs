c13cce9672636106b4b92a958e9db1e4ff0a5b2e
Derek <derek.heidorn@gov.bc.ca>
2019-07-16 12:53:30 -0700

    Unit test updates

--- jrcc-access-spring-boot-autoconfigure/src/test/java/ca/gov/bc/open/jrccaccess/autoconfigure/plugins/rabbitmq/RabbitMqDocumentInputTester.java
+++ jrcc-access-spring-boot-autoconfigure/src/test/java/ca/gov/bc/open/jrccaccess/autoconfigure/plugins/rabbitmq/RabbitMqDocumentInputTester.java
@@ -10,7 +10,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.amqp.AmqpRejectAndDontRequeueException;
-import org.springframework.amqp.ImmediateAcknowledgeAmqpException;
 
 import ca.gov.bc.open.jrccaccess.autoconfigure.AccessProperties;
 import ca.gov.bc.open.jrccaccess.autoconfigure.AccessProperties.PluginConfig;
@@ -19,6 +18,7 @@ import ca.gov.bc.open.jrccaccess.autoconfigure.services.DocumentReadyHandler;
 import ca.gov.bc.open.jrccaccess.libs.DocumentReadyMessage;
 import ca.gov.bc.open.jrccaccess.libs.DocumentStorageProperties;
 import ca.gov.bc.open.jrccaccess.libs.TransactionInfo;
+import ca.gov.bc.open.jrccaccess.libs.services.exceptions.DocumentDigestMatchFailedException;
 import ca.gov.bc.open.jrccaccess.libs.services.exceptions.DocumentMessageException;
 import ca.gov.bc.open.jrccaccess.libs.services.exceptions.ServiceUnavailableException;
 import ca.gov.bc.open.jrccaccess.libs.utils.DigestUtils;
@@ -27,6 +27,7 @@ public class RabbitMqDocumentInputTester {
 
 
 	private static final String SERVICE_UNAVAILABLE_EXCEPTION = "ServiceUnavailableException";
+	private static final String DIGEST_FAILED_EXCEPTION = "DigestFailedException";
 
 	private RabbitMqDocumentInput sut;
 	
@@ -53,6 +54,7 @@ public class RabbitMqDocumentInputTester {
 		Mockito.doNothing().when(this.documentReadyService).publish(Mockito.any());
 		Mockito.doNothing().when(documentReadyHandlerMock).handle(Mockito.anyString(), Mockito.anyString());
 		Mockito.doThrow(ServiceUnavailableException.class).when(documentReadyHandlerMock).handle(Mockito.anyString(), Mockito.eq(SERVICE_UNAVAILABLE_EXCEPTION));
+		Mockito.doThrow(DocumentDigestMatchFailedException.class).when(documentReadyHandlerMock).handle(Mockito.anyString(), Mockito.eq(DIGEST_FAILED_EXCEPTION));
 		Mockito.when(this.storageService.putString(Mockito.anyString())).thenReturn(new DocumentStorageProperties("key", "A1"));
 		
 		PluginConfig output = new PluginConfig();
@@ -91,14 +93,31 @@ public class RabbitMqDocumentInputTester {
 		DocumentStorageProperties storageProperties = new DocumentStorageProperties(key, md5);
 		
 		// we want to throw ServiceUnavailableException, which will then cause it to throw an AmqpRejectAndDontRequeueException
-		Mockito.doThrow(ServiceUnavailableException.class).when(documentReadyHandlerMock).handle(Mockito.anyString(), Mockito.anyString());
-		Mockito.when(this.transactionInfoMock.getSender()).thenReturn("DefaultSender");
+		Mockito.when(this.transactionInfoMock.getSender()).thenReturn(SERVICE_UNAVAILABLE_EXCEPTION);
 		Mockito.when(this.message.getTransactionInfo()).thenReturn(transactionInfoMock);
 		Mockito.when(this.message.getDocumentStorageProperties()).thenReturn(storageProperties);
 		Mockito.when(this.storageService.putString(Mockito.anyString())).thenReturn(new DocumentStorageProperties(key, md5));
 		Mockito.when(this.storageService.getString(Mockito.anyString(), Mockito.anyString())).thenReturn(textContent);
 		
-		sut = new RabbitMqDocumentInput(documentReadyHandlerMock, storageService);
+		sut.receiveMessage(message);
+		
+	}
+	
+	@Test(expected = AmqpRejectAndDontRequeueException.class)
+	public void when_DocumentDigestMatchFailedException_should_throw_AmqpRejectAndDontRequeueException() throws Exception {
+		
+		String key = RandomHelper.makeRandomString(10);
+		String textContent = RandomHelper.makeRandomString(20);
+		String md5 = DigestUtils.computeMd5(textContent);
+		
+		DocumentStorageProperties storageProperties = new DocumentStorageProperties(key, md5);
+		
+		// we want to throw ServiceUnavailableException, which will then cause it to throw an AmqpRejectAndDontRequeueException
+		Mockito.when(this.transactionInfoMock.getSender()).thenReturn(DIGEST_FAILED_EXCEPTION);
+		Mockito.when(this.message.getTransactionInfo()).thenReturn(transactionInfoMock);
+		Mockito.when(this.message.getDocumentStorageProperties()).thenReturn(storageProperties);
+		Mockito.when(this.storageService.putString(Mockito.anyString())).thenReturn(new DocumentStorageProperties(key, md5));
+		Mockito.when(this.storageService.getString(Mockito.anyString(), Mockito.anyString())).thenReturn(textContent);
 		
 		sut.receiveMessage(message);
 		
