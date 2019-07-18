74a0ae687a3879ecea236735acaf8749fb7be4de
Chris Hoban <christopher.hoban@nttdata.com>
2019-07-18 16:09:39 -0700

    Trying new assemble script

--- .s2i/bin/assemble.sh
+++ .s2i/bin/assemble.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-mvn clean install
+
+# restore build artifacts
+if [ "$(ls /tmp/s2i/artifacts/ 2>/dev/null)" ]; then
+    mv /tmp/s2i/artifacts/* $HOME/.
+fi
+
+# move the application source
+mv /tmp/s2i/src $HOME/src
+
+# build application artifacts
+pushd ${HOME}
+make all
+
+# install the artifacts
+make install
+popd
+
 mvn clean install -P sample-app
