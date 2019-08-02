5b06483c5443970892547dbf93b8f8f8ebd4735c
alexjoybc <alexandre.joyeux@gov.bc.ca>
2019-08-02 14:29:35 -0700

    feat(sftp): configure local

--- jrcc-access-spring-boot-autoconfigure/src/main/java/ca/bc/gov/open/jrccaccess/autoconfigure/plugins/sftp/AutoConfiguration.java
+++ jrcc-access-spring-boot-autoconfigure/src/main/java/ca/bc/gov/open/jrccaccess/autoconfigure/plugins/sftp/AutoConfiguration.java
@@ -1,9 +1,90 @@
 package ca.bc.gov.open.jrccaccess.autoconfigure.plugins.sftp;
 
+import com.jcraft.jsch.ChannelSftp;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.annotation.Poller;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.core.MessageSource;
+import org.springframework.integration.file.filters.AcceptOnceFileListFilter;
+import org.springframework.integration.file.remote.session.CachingSessionFactory;
+import org.springframework.integration.file.remote.session.SessionFactory;
+import org.springframework.integration.sftp.filters.SftpSimplePatternFileListFilter;
+import org.springframework.integration.sftp.inbound.SftpInboundFileSynchronizer;
+import org.springframework.integration.sftp.inbound.SftpInboundFileSynchronizingMessageSource;
+import org.springframework.integration.sftp.session.DefaultSftpSessionFactory;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessagingException;
+
+import javax.websocket.MessageHandler;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Configuration
 @ComponentScan
+@ConditionalOnProperty(
+        value="bcgov.access.input.plugin",
+        havingValue = "sftp"
+)
 public class AutoConfiguration {
+
+
+    @Bean
+    public SessionFactory<ChannelSftp.LsEntry> sftpSessionFactory() {
+        DefaultSftpSessionFactory factory = new DefaultSftpSessionFactory(true);
+        factory.setHost("localhost");
+        factory.setPort(2222);
+        factory.setUser("master");
+        factory.setPassword("master");
+        factory.setAllowUnknownKeys(true);
+        return new CachingSessionFactory<ChannelSftp.LsEntry>(factory);
+    }
+
+
+    @Bean
+    public SftpInboundFileSynchronizer sftpInboundFileSynchronizer() {
+        SftpInboundFileSynchronizer fileSynchronizer = new SftpInboundFileSynchronizer(sftpSessionFactory());
+        fileSynchronizer.setDeleteRemoteFiles(false);
+        fileSynchronizer.setRemoteDirectory("/home/master/");
+        fileSynchronizer.setFilter(new SftpSimplePatternFileListFilter("*.xml"));
+        return fileSynchronizer;
+    }
+
+    @Bean
+    @InboundChannelAdapter(channel = "sftpChannel", poller = @Poller(fixedDelay = "5000"))
+    public MessageSource<File> sftpMessageSource() {
+        SftpInboundFileSynchronizingMessageSource source =
+                new SftpInboundFileSynchronizingMessageSource(sftpInboundFileSynchronizer());
+        source.setLocalDirectory(new File("C:/Users/177226/Downloads/Justice"));
+        source.setAutoCreateLocalDirectory(true);
+        source.setLocalFilter(new AcceptOnceFileListFilter<File>());
+        source.setMaxFetchSize(1);
+        return source;
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "sftpChannel")
+    public MessageHandler handler() {
+        return new MessageHandler() {
+
+            public void handleMessage(Message<File> message) throws MessagingException {
+
+
+
+                try {
+                    System.out.println(new String(Files.readAllBytes(Paths.get(message.getPayload().getPath()))));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        };
+    }
+
+
 }
